12b4335dd274331e757d970cd7aa66fe46e923e0
Eshan Maurya <90308669+EshanMaurya@users.noreply.github.com>
2025-09-20 06:12:54 +0000

    Fixed Median bug

--- QuizMarksAnalysis.py
+++ QuizMarksAnalysis.py
@@ -13,25 +13,25 @@ st.subheader("Dataset")
 st.dataframe(data)
 
 # Let us find the average, median and modal marks.
-
+quiz_marks = pd.to_numeric(data['QUIZ 1'], errors='coerce').dropna()
 st.subheader("Statistical Analysis")
-st.write("Average Marks: ", np.mean(data['QUIZ 1']))
-st.write("Median Marks: ", np.median(data['QUIZ 1']))
-st.write("Mode Marks: ", data['QUIZ 1'].mode()[0])
+st.write("Average Marks: ", np.mean(quiz_marks))
+st.write("Median Marks: ", np.median(quiz_marks))
+st.write("Mode Marks: ", quiz_marks.mode()[0])
 
 # Let us visualize the marks via histogram.
 
 st.subheader("Marks Distribution")
-st.bar_chart(data['QUIZ 1'].value_counts().sort_index())
-st.line_chart(data['QUIZ 1'].value_counts().sort_index())
-st.area_chart(data['QUIZ 1'].value_counts().sort_index())
+st.bar_chart(quiz_marks.value_counts().sort_index())
+st.line_chart(quiz_marks.value_counts().sort_index())
+st.area_chart(quiz_marks.value_counts().sort_index())
 
 # creating a percentile calculator
 
 st.subheader("Percentile Calculator")
 marks = st.number_input("Enter the marks to find the percentile: ", min_value  = 0, max_value = 20, step= 2)
 if st.button("Calculate Percentile"):
-    percentile = (data['QUIZ 1'] < marks).sum() / len(data) * 100
+    percentile = (quiz_marks < marks).sum() / len(data) * 100
     st.write(f"The percentile for marks {marks} is {percentile:.2f}%")
     st.balloons()
 
